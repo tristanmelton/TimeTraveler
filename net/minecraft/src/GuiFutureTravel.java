64d282085c2b1f3d08da4c61632cc99f06558112
Tristan Melton <charsmud@gmail.com>
2013-01-10 08:36:02 -0800

    Number Acceptance may be working, untested

--- net/minecraft/src/GuiFutureTravel.java
+++ net/minecraft/src/GuiFutureTravel.java
@@ -75,12 +75,15 @@ public class GuiFutureTravel extends GuiScreen{
      */
     protected void keyTyped(char par1, int par2)
     {
-        this.theGuiTextField.textboxKeyTyped(par1, par2);
+    	if(!Keyboard.isKeyDown(Keyboard.KEY_0) || !Keyboard.isKeyDown(Keyboard.KEY_1) || !Keyboard.isKeyDown(Keyboard.KEY_2) || !Keyboard.isKeyDown(Keyboard.KEY_3)|| !Keyboard.isKeyDown(Keyboard.KEY_4) || !Keyboard.isKeyDown(Keyboard.KEY_5) || !Keyboard.isKeyDown(Keyboard.KEY_6) || !Keyboard.isKeyDown(Keyboard.KEY_7)|| !Keyboard.isKeyDown(Keyboard.KEY_8) || !Keyboard.isKeyDown(Keyboard.KEY_9))
+    	{
+    		System.out.println("Do Nothing");
+    	}
+    	else
+    	{
+            this.theGuiTextField.textboxKeyTyped(par1, par2);
+    	}
         ((GuiButton)this.controlList.get(0)).enabled = this.theGuiTextField.getText().trim().length() > 0;
-        if(!Keyboard.isKeyDown(2) | !Keyboard.isKeyDown(3) | !Keyboard.isKeyDown(4) | !Keyboard.isKeyDown(5) | !Keyboard.isKeyDown(6) | !Keyboard.isKeyDown(7) | !Keyboard.isKeyDown(8) | !Keyboard.isKeyDown(9) | !Keyboard.isKeyDown(10) | !Keyboard.isKeyDown(11))
-        {
-        	System.out.println(":)");
-        }
         if (par1 == 13)
         {
             this.actionPerformed((GuiButton)this.controlList.get(0));
